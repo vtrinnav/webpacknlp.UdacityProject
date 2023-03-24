04e70281d38ca1f062fb2ac28ee9a6038917c7ee
vtrinnav <vtrindadenavarro@gmail.com>
2023-03-24 11:25:17 -0400

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports={
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    output: {}
 
 };
+
+module: {
+    rules: [
+            {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            }
+    ]
+};
+
+
