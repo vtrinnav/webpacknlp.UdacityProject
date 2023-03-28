8b4c28f46e7435ed95636f015c3d0c16d09a7a3a
vtrinnav <vtrindadenavarro@gmail.com>
2023-03-28 11:18:51 -0400

    commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const { required } = require("nodemon/lib/config");
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports={
     mode: 'development',
@@ -21,10 +22,14 @@ module.exports={
         new HtmlWebPackPlugin({
             template: "./src/client/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
     ]
-
-
 };
 
 
