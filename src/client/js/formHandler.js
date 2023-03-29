01c1ce1780a7d24e1fc70e98282014ec125e5890
vtrinnav <vtrindadenavarro@gmail.com>
2023-03-28 20:05:45 -0400

    commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,13 +2,15 @@ export function handleSubmit(event) {
     event.preventDefault() 
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value 
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
+    fetch('http://localhost:8081/test')
+    .then(res => {
+        return res.json()
+    })
+    .then(function(data) {
         document.getElementById('results').innerHTML = res.message
     })
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 var path = require('path');
 const express = require('express');
+const port = 8081;
 const mockAPIResponse=require('./mockAPI');
 
 const app = express();
@@ -12,8 +13,8 @@ app.get('/', function(req,res){
     res.sendFile('dist/index.html')
 });
 
-app.listen(5050, function (){
-    console.log('app listening on port 5050!')
+app.listen(8081, function (){
+    console.log('app listening on port 8081!')
 });
 
 app.get('/test', function(req,res){
