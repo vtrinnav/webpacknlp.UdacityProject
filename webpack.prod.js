c93f5a38865199198f108493e62dde335083776c
vtrinnav <vtrindadenavarro@gmail.com>
2023-03-30 11:41:43 -0400

    commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,19 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 
 module.exports={
     mode: 'production',
     devtool: 'source-map',
     entry: './src/client/index.js',
+    optimization:{
+        minimizer:[new TerserPlugin({}), new CssMinimizerPlugin({})],
+    },
     output:{
         libraryTarget: 'var',
         library: 'Client'
@@ -32,7 +39,8 @@ module.exports={
             template: "./src/client/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new MiniCssExtractPlugin({filename: "[name].css"}),
+        new WorkboxPlugin.GenerateSW(),
 
     ]
 
